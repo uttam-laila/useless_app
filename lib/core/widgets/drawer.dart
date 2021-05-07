015a7f85a7221e1d0d6e02d3d1b70ec69501fe8b
Uttam Laila <uttam.laila@gmail.com>
2021-05-07 19:56:26 +0530

    Added Icon

--- lib/core/widgets/drawer.dart
+++ lib/core/widgets/drawer.dart
@@ -5,22 +5,21 @@ class UselessDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: MediaQuery.of(context).size.width / 2,
+      height: MediaQuery.of(context).size.height,
       child: Drawer(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            DrawerHeader(
-              child: Center(child: Text('Useless App')),
-            ),
-            ListTile(
-              title: Center(
-                  child: Column(
-                children: [
-                  Text('This app is designed and developed by'),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: InkWell(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              DrawerHeader(
+                child: Center(child: Text('Useless App')),
+              ),
+              ListTile(
+                title: Center(
+                    child: Column(
+                  children: [
+                    Text('This app is designed and developed by'),
+                    ListTile(
                       onLongPress: () {
                         Clipboard.setData(ClipboardData(
                                 text:
@@ -31,54 +30,66 @@ class UselessDrawer extends StatelessWidget {
                           Navigator.pop(context);
                         });
                       },
-                      child: Text(
-                        'Uttam Laila',
-                        style: Theme.of(context)
-                            .textTheme
-                            .subtitle1!
-                            .copyWith(fontWeight: FontWeight.bold),
+                      title: Center(
+                        child: Text(
+                          'Uttam Laila',
+                          style:
+                              Theme.of(context).textTheme.subtitle1!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                        ),
                       ),
                     ),
-                  ),
-                  Text(
-                      'This app uses Provider to maintain the states. You can get the source code from the below link.'),
-                ],
-              )),
-            ),
-            ListTile(
-              title: Text('App icon designed by Vinneth J'),
-              onTap: () {
-                Clipboard.setData(ClipboardData(
-                        text: 'https://www.instagram.com/_vinu_creation_/'))
-                    .then((_) {
-                  Scaffold.of(context).showSnackBar(
-                      SnackBar(content: Text("Profile copied to clipboard")));
-                  Navigator.pop(context);
-                });
-              },
-            ),
-            ListTile(
-              title: Center(child: Text('Github Link')),
-              onTap: () {
-                Clipboard.setData(ClipboardData(
-                        text: 'https://github.com/uttam-laila/useless_app'))
-                    .then((_) {
-                  Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text("Github address copied to clipboard")));
-                  Navigator.pop(context);
-                });
-              },
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Center(child: Text('Version: 1.0.0+1')),
-                SizedBox(
-                  height: 12,
+                    Text(
+                        'This app is coded in Flutter (Dart) and uses Provider to maintain the states. You can get the source code from the below link.'),
+                  ],
+                )),
+              ),
+              ListTile(
+                title: Center(child: Text('Github Link')),
+                onTap: () {
+                  Clipboard.setData(ClipboardData(
+                          text: 'https://github.com/uttam-laila/useless_app'))
+                      .then((_) {
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text("Github address copied to clipboard")));
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              ListTile(
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('App icon designed by '),
+                    Text(
+                      'Vineeth J',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                  ],
                 ),
-              ],
-            )
-          ],
+                onTap: () {
+                  Clipboard.setData(ClipboardData(
+                          text: 'https://www.instagram.com/_vinu_creation_/'))
+                      .then((_) {
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text("Profile copied to clipboard")));
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Center(child: Text('Version: 1.0.0+1')),
+                  SizedBox(
+                    height: 12,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
